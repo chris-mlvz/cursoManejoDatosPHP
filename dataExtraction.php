877d5df09a03e3ba9356442af0e25ff4c2884511
chris_mlvz <chris.mlvz@gmail.com>
2021-06-23 19:54:41 -0500

    Data extracction PHP

---
+++ dataExtraction.php
@@ -0,0 +1,24 @@
+<?php
+
+// $data = "Estudio PHP";
+// echo $data[0];
+
+// $post = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, odio";
+// $extract = substr($post, 0, 20);
+
+// echo "$extract... [ver más]";
+
+// $data = "javascript,php,laravel"; // campo tags
+// $tags = explode(",", $data); // array
+// echo "<pre>";
+// var_dump($tags);
+// echo "</pre>";
+
+// $courses = ["javascript", "php", "laravel"];
+// echo implode(", ", $courses);
+
+$course = "       Curso de PHP         ";
+$course  = trim($course);
+echo "<pre>";
+echo "Quiero aprender $course, y otro texto";
+echo "</pre>";
