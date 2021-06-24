5c79ac8a9220e441d60e326eea6adb307f5c9276
chris_mlvz <chris.mlvz@gmail.com>
2021-06-24 09:49:48 -0500

    reviewing project, phpunit

--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -13,4 +13,22 @@ class validateTest extends TestCase
         $email = Validate::email('i@@rimorsoft.com');
         $this->assertFalse($email);
     }
+
+    public function test_url()
+    {
+        $url = Validate::url('https://platizi.com');
+        $this->assertTrue($url);
+
+        $url = Validate::url('platizi.com');
+        $this->assertFalse($url);
+    }
+
+    public function test_password()
+    {
+        $password = Validate::password('012345');
+        $this->assertTrue($password);
+
+        $password = Validate::password('asdasd12');
+        $this->assertFalse($password);
+    }
 }
